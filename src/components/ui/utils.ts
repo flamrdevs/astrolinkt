5c957a812bbcd2a2af35dba883f6ce2d7362fe26
flamrdevs <flamrdevs@gmail.com>
2024-03-17 02:59:46 +0700

    chore: klass next

--- src/components/ui/utils.ts
+++ src/components/ui/utils.ts
@@ -1,21 +1,18 @@
-import setupCore from "@klass/core/setup";
-import type { Config } from "@klass/core/setup";
-import setupCoreGroup from "@klass/core/group/setup";
-import setupReact from "@klass/react/setup";
-import * as setupMonoReact from "@klass/react/mono/setup";
+import { createKlass, createReklass } from "@klass/core/create";
+import { createGroup } from "@klass/core/group/create";
+import * as poly from "@klass/react/create";
+import * as mono from "@klass/react/mono/create";
 
-import { twMerge } from "tailwind-merge";
+import { twMerge as end } from "tailwind-merge"; // optional
 
 import clsx from "clsx";
 import type { ClassValue } from "clsx";
 
-const config: Config = {
-  end: twMerge,
-};
+export const cn = (...classValues: ClassValue[]) => end(clsx(classValues));
 
-export const cn = (...classValues: ClassValue[]) => twMerge(clsx(classValues));
-
-export const [klass, reklass] = setupCore(config);
-export const [klassed, reklassed] = setupReact(config);
-export const group = setupCoreGroup(config);
-export const mklassed = setupMonoReact.klassed(config);
+export const klass = createKlass({ end });
+export const reklass = createReklass({ end });
+export const klassed = poly.createKlassed(klass);
+export const reklassed = poly.createReklassed(reklass);
+export const group = createGroup(klass);
+export const mklassed = mono.createKlassed(klass);
