2a1bd6a5234ded9cb0bca1738006882795c73f36
flamrdevs <flamrdevs@gmail.com>
2024-03-26 19:49:54 +0700

    build: true optional package

--- src/components/ui/utils.ts
+++ src/components/ui/utils.ts
@@ -3,18 +3,18 @@ import { createGroup } from "@klass/core/group/create";
 import * as poly from "@klass/react/create";
 import * as mono from "@klass/react/mono/create";
 
-import { twMerge as end } from "tailwind-merge"; // optional package
+// import { twMerge as end } from "tailwind-merge"; // optional package, install first
 
 import clsx from "clsx";
 import type { ClassValue } from "clsx";
 
-export const cn = (...classValues: ClassValue[]) => end(clsx(classValues));
-// export const cn = (...classValues: ClassValue[]) => clsx(classValues); // without tailwind-merge
+export const cn = (...classValues: ClassValue[]) => clsx(classValues);
+// export const cn = (...classValues: ClassValue[]) => end(clsx(classValues)); // with tailwind-merge
 
-export const klass = createKlass({ end });
-export const reklass = createReklass({ end });
-// export const klass = createKlass(); // without tailwind-merge
-// export const reklass = createReklass(); // without tailwind-merge
+export const klass = createKlass();
+export const reklass = createReklass();
+// export const klass = createKlass({ end }); // with tailwind-merge
+// export const reklass = createReklass({ end }); // with tailwind-merge
 export const klassed = poly.createKlassed(klass);
 export const reklassed = poly.createReklassed(reklass);
 export const group = createGroup(klass);
